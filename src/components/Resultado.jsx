0c1c668c845405d97d3c1b6e5945d8d9b86651da
Tuquina <fernandotuquina@gmail.com>
2022-07-29 23:27:18 -0300

    Consume la API para mostrar las cotizaciones

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,10 +1,29 @@
 import styled from "@emotion/styled"
 
+const Contenedor = styled.div`
+    color: #FFF;
+`
+
+const Texto = styled.p`
+
+`
+
+const Precio = styled.p`
+
+`
+
 const Resultado = ({resultado}) => {
-  return (
-    <div>
-        <h1>Desde resultado</h1>
-    </div>
+  
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado
+
+    return (
+    <Contenedor>
+        <Precio>El Precio es de: <span>{PRICE}</span></Precio>
+        <Texto>El Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+        <Texto>El Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+        <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+        <Texto>Última Actualización: <span>{LASTUPDATE}</span></Texto>
+    </Contenedor>
   )
 }
 
