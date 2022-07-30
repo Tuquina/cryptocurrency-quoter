b87827a680b349b68676fe030a3bcd1b0f8845de
Tuquina <fernandotuquina@gmail.com>
2022-07-29 23:34:18 -0300

    Estilos para mostrar el resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -2,14 +2,31 @@ import styled from "@emotion/styled"
 
 const Contenedor = styled.div`
     color: #FFF;
+    font-family: 'Lato', sans-serif;
+
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+`
+
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
 `
 
 const Texto = styled.p`
-
+    font-size: 18px;
+    span {
+        font-weight: 700;
+    }
 `
 
 const Precio = styled.p`
-
+    font-size: 24px;
+    span {
+        font-weight: 700;
+    }
 `
 
 const Resultado = ({resultado}) => {
@@ -18,11 +35,14 @@ const Resultado = ({resultado}) => {
 
     return (
     <Contenedor>
-        <Precio>El Precio es de: <span>{PRICE}</span></Precio>
-        <Texto>El Precio más alto del día: <span>{HIGHDAY}</span></Texto>
-        <Texto>El Precio más bajo del día: <span>{LOWDAY}</span></Texto>
-        <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
-        <Texto>Última Actualización: <span>{LASTUPDATE}</span></Texto>
+        <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt="Imagen Criptomoneda" />
+        <div>
+            <Precio>El Precio es de: <span>{PRICE}</span></Precio>
+            <Texto>El Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+            <Texto>El Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+            <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+            <Texto>Última Actualización: <span>{LASTUPDATE}</span></Texto>
+        </div>
     </Contenedor>
   )
 }
